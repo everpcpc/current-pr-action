82582375e1a0f87d0e8beb21011a6fab93c64f75
everpcpc <git@everpcpc.com>
2022-06-09 12:59:46 +0800

    z

--- src/index.js
+++ src/index.js
@@ -9,14 +9,23 @@ const { Octokit } = require('@octokit/rest');
 const token = core.getInput('token');
 const octokit = new Octokit({ auth: `token ${token}` });
 const context = github.context;
-
+const reREF = /^refs\/pull\/(\d+)\/merge$/;
 
 async function run() {
     try {
-        // const owner = context.repo.owner;
-        // const repo = context.repo.repo;
-        const ref = context.ref
-        core.setOutput('url', ref);
+        const match = context.ref.match(reREF);
+        if (match === null) {
+            core.setFailed("current ref is not pr");
+            return
+        }
+        const prID = match[1];
+        const pr = await octokit.pulls.get({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            pull_number: prID,
+        })
+        core.setOutput('url', pr.url);
+        core.setOutput('title', pr.data.title);
     } catch (error) {
         core.setFailed(error.message);
     }
